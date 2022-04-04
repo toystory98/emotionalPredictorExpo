eee397a3e95bff3a43a471272142f804c91fcb36
toystory <chaninat.b@hotmail.com>
2022-04-04 22:38:39 +0700

    combine

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
 import { Audio } from 'expo-av';
-import base64 from 'react-native-base64'
+import React, { useState } from "react";
+import { Block, Text } from 'galio-framework';
+import axios from 'axios';
+import Feather from "react-native-vector-icons/Feather";
+import { TextLoader } from 'react-native-indicator';
+import * as DocumentPicker from 'expo-document-picker';
+
+Feather.loadFont();
+import {
+  SafeAreaView,
+  StyleSheet,
+  useColorScheme,
+  Image,
+  Dimensions,
+  TouchableOpacity
+} from 'react-native';
+
+import {
+  Colors
+} from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
-  const [recording, setRecording] = React.useState();
-  const [audio, setAudio] = React.useState();
-  const [message, setMessage] = React.useState("");
+  const [countOnPress, setCountOnPress] = useState(0);
+  const [imagePath, setImagePath] = useState(require('./picture/record-unpress.png'));
+  const [recordFlag, setRecordFlag] = useState(true);
+  const [captionText, setCaptionText] = useState(`press button to record \nor open file to look emotional`);
+  const isDarkMode = useColorScheme() === 'light';
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  const [recording, setRecording] = useState();
+  const [axiosData, setAxiosData] = useState(null);
+  const [fromAxios, setFromAxios] = useState(false);
+  const [isFetch, setIsFetch] = useState(false);
 
   async function startRecording() {
     try {
@@ -25,7 +50,7 @@ export default function App() {
 
         setRecording(recording);
       } else {
-        setMessage("Please grant permission to app to access microphone");
+        alert("Please grant permission to app to access microphone");
       }
     } catch (err) {
       console.error('Failed to start recording', err);
@@ -35,90 +60,115 @@ export default function App() {
   async function stopRecording() {
     setRecording(undefined);
     await recording.stopAndUnloadAsync();
-    const { sound, status } = await recording.createNewLoadedSoundAsync();
-    setAudio({
-      sound: sound,
-      duration: getDurationFormatted(status.durationMillis),
-      file: recording.getURI()
-    })
+    await recording.createNewLoadedSoundAsync();
+    await goForAxios(recording.getURI())
 
-    // utitlity function to convert BLOB to BASE64
-    const blobToBase64 = (blob) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(blob);
-      return new Promise((resolve) => {
-        reader.onloadend = () => {
-          resolve(reader.result);
-        };
-      });
-    };
-
-    // Fetch audio binary blob data
-
-    const audioURI = recording.getURI();
-    const blob = await new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.onload = function () {
-        resolve(xhr.response);
-      };
-      xhr.onerror = function (e) {
-        reject(new TypeError("Network request failed"));
-      };
-      xhr.responseType = "blob";
-      xhr.open("GET", audioURI, true);
-      xhr.send(null);
-    });
-
-    const audioBase64 = await blobToBase64(blob);
-
-    // const response = await axios.post(
-    //   "http://192.18.0.4:8000/audio",
-    //   base64.encode(audioBase64)
-    // );
-    console.log(base64.encode(audioBase64));
-
-    // We're done with the blob and file uploading, close and release it
-    blob.close()
-
+    // console.log(recording.getURI());
   }
 
-  function getDurationFormatted(millis) {
-    const minutes = millis / 1000 / 60;
-    const minutesDisplay = Math.floor(minutes);
-    const seconds = Math.round((minutes - minutesDisplay) * 60);
-    const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
-    return `${minutesDisplay}:${secondsDisplay}`;
+  async function goForAxios(uri) {
+    setFromAxios(true);
+    setIsFetch(true);
+    setCaptionText('Processing');
+    axios.get("http://localhost:3001/predictor")
+      .then(response => {
+        console.log('getting data from axios', response.data.result);
+        console.log(uri);
+        setTimeout(() => {
+          setIsFetch(false);
+          setAxiosData(require('./picture/happy.png'));
+          setCaptionText('Happy');
+        }, 2000)
+      })
+      .catch(error => {
+        setIsFetch(false);
+        setAxiosData(require('./picture/error.png'));
+        setCaptionText('Please try again');
+        console.log(error);
+      });
+  };
+
+  async function onPressButton() {
+    console.log("button pressed")
+    setRecordFlag(!recordFlag);
+    if (recordFlag == true) {
+      setFromAxios(false)
+      setCountOnPress(countOnPress + 1);
+      setImagePath(require('./picture/record-press.png'));
+      setCaptionText('recording');
+      await startRecording();
+    } else {
+      setAxiosData(null);
+      setImagePath(require('./picture/record-unpress.png'));
+      setCaptionText(`press button to record \nor open file to look emotional`);
+      if (countOnPress > 0) {
+        await stopRecording();
+      }
+    }
+  };
+
+  async function pickDocument() {
+    let result = await DocumentPicker.getDocumentAsync({ type: 'audio/*' });
+    console.log(result);
   }
 
   return (
-    <View style={styles.container}>
-      <Text>{message}</Text>
-      <Button
-        title={recording ? 'Stop Recording' : 'Start Recording'}
-        onPress={recording ? stopRecording : startRecording} />
-      <Button style={styles.button} onPress={() => audio.sound.replayAsync()} title="Play"></Button>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={backgroundStyle}>
+      <Block height={50} safe fluid />
+      <Block middle style={[styles.card, { backgroundColor: isDarkMode ? "#F3F3F3" : null }]}>
+        <Block middle height={300}>
+          {fromAxios ?
+            <Image style={styles.imageCard} source={axiosData} /> :
+            <Image style={styles.imageCard} source={require('./picture/landing.png')} />
+          }
+        </Block>
+        <Block center height={80}>
+          {recordFlag && !isFetch ?
+            <Text p bold center style={{ marginTop: 30 }}>
+              {captionText}
+            </Text> :
+            <TextLoader text={captionText} textStyle={styles.textStatus} />
+          }
+        </Block>
+      </Block>
+      <Block height={80} safe center fluid style={{ marginTop: 50 }}>
+        <TouchableOpacity onPress={onPressButton}>
+          <Image style={styles.imageButton} source={imagePath} />
+        </TouchableOpacity>
+      </Block>
+      <Block height={200} safe>
+        <Text p bold center style={{ marginTop: 80, color: isDarkMode ? "#F3F3F3" : "#727272" }}
+          onPress={recordFlag? pickDocument : null} 
+        >
+          open from file
+        </Text>
+      </Block>
+      <Block height={height} safe />
+    </SafeAreaView>
   );
 }
 
+const { height, width } = Dimensions.get('screen');
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  card: {
+    marginHorizontal: 25,
+    borderRadius: 25,
+    height: 450
   },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+  imageCard: {
+    height: 270,
+    width: 270,
+    resizeMode: 'contain'
   },
-  fill: {
-    flex: 1,
-    margin: 16
+  imageButton: {
+    height: 100,
+    width: 100,
+    resizeMode: 'contain'
   },
-  button: {
-    margin: 16
+  textStatus: {
+    marginTop: 30,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20
   }
 });
